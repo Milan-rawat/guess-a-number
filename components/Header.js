49c65b56085fc706418e248e8452af53166d5d5f
Milan Rawat <milanrawat086@gmail.com>
2021-07-13 17:19:51 +0530

    some colors changed and made a colors file

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import Colors from "../constants/colors";
+
 const Header = (props) => {
   return (
     <View style={styles.header}>
@@ -14,7 +16,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: "#f7287b",
+    backgroundColor: Colors.primary,
     alignItems: "center",
   },
   headerTitle: {
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 import Card from "../components/Card";
+import Colors from "../constants/colors";
 
 const StartGameScreen = (props) => {
   return (
@@ -11,8 +12,12 @@ const StartGameScreen = (props) => {
         <Text>Select a Number</Text>
         <TextInput style={{ borderColor: "black" }} />
         <View style={styles.buttonContainer}>
-          <Button title="Reset" onPress={() => {}} />
-          <Button title="Confirm" onPress={() => {}} />
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Confirm" onPress={() => {}} color={Colors.primary} />
+          </View>
         </View>
       </Card>
     </View>
@@ -39,6 +44,9 @@ const styles = StyleSheet.create({
     width: "100%",
     justifyContent: "space-between",
     paddingHorizontal: 15,
+  },
+  button: {
+    width: 100,
   },
 });
 
